c27ada4897c0e409adb8541ed55fc6bdd96a08a4
timothma <macnary17@up.edu>
2015-11-09 22:30:05 -0800

    Homework 5d turn in with TickTackToe

--- app/src/main/java/edu/up/cs301/ConnectFour/ConnectFourState.java
+++ app/src/main/java/edu/up/cs301/ConnectFour/ConnectFourState.java
@@ -7,14 +7,15 @@ import edu.up.cs301.game.infoMsg.GameState;
  * Created by phamj17 on 11/5/2015.
  */
 public class ConnectFourState extends GameState {
-//what's up
+    //what's up
     private final int EMPTY = 0;
     //player 1 piece
     private final int RED = 1;
     //player 2/computer player piece
     private final int BLACK = 2;
-
-
+    //Who is the winner
+    private int winner = -1;
+    //boolean that is true if game is over
     private boolean gameOver;
     //0 for player 1, 1 for player 2/computer player
     private int turn;
@@ -23,60 +24,265 @@ public class ConnectFourState extends GameState {
     private int[] column = new int[7];
 
     //7 columns, 6 rows
-    private int[][] board = new int[7][6];
+    private int[][] board = new int[6][7];
 
-
+    //constructor that initializes board
     public ConnectFourState() {
+        //loop through the entire board
         for (int i = 0; i < board.length; i++) {
             column[i] = 0;
             for (int j = 0; j < board[i].length; j++) {
                 board[i][j] = EMPTY;
             }
         }
-
+        //set the turn
         turn = 0;
+        //the game is not over...
         gameOver = false;
-
     }
 
+    //copy constructor
     public ConnectFourState(ConnectFourState state) {
         board = state.getBoard();
-        gameOver= state.getGameOver();
+        gameOver = state.getGameOver();
         turn = state.getTurn();
         column = state.getColumn();
-
-
     }
 
-    public int getPiece(int row, int col){
+    //get a piece...
+    public int getPiece(int row, int col) {
         //check if row or col is a neg number
-        if(board == null || row < 0 || col < 0) return -1;
+        if (board == null || row < 0 || col < 0) return -1;
         //check if row or col is outside the board
-        if(row >= board.length || col >= board[row].length) return -1;
+        if (row >= board.length || col >= board[row].length) return -1;
         return board[row][col];
     }
 
-    public void setPiece(int row, int col, int turn){
-        //check if row or col is a neg number
-        if(board == null || row < 0 || col < 0) return;
-        //check if row or col is outside the board
-        if(row >= board.length || col >= board[row].length) return;
-        //if it's player 1's turn, set the piece to a red piece
-        //change turn to other player
-        if(turn == 0){
-            board[row][col] = RED;
-            this.turn = 1;
+    //set a piece...
+    public boolean setPiece(int col) {
+        //temp variables
+        int[][] tempBoard = getBoard();
+        int[] tempCol = getColumn();
+        //check if board exists, if piece is out of bounds
+        if (tempBoard == null || col < 0 || col > 6) {
+            return false;
         }
-        //if it's player 2's turn, set the piece to a black piece
-        //change turn to other player
-        if(turn == 1){
-            board[row][col] = BLACK;
-            this.turn = 0;
+
+        //the column is already full, so return false
+        if (tempCol[col] >= 6) {
+            return false;
+        }
+        //else, do this stuff
+        else {
+            int tempRow = tempCol[col];
+            //if it's player 1's turn, set the piece to red
+            //change the turn
+            if (turn == 0) {
+                tempBoard[col][tempRow] = RED;
+                //turn = 1;
+            }
+            //else if it's player 2's turn, set the piece to black
+            //change the turn
+            else if (turn == 1) {
+                tempBoard[col][tempRow] = BLACK;
+                //turn = 0;
+            }
+
+            //update our column array and the board
+            tempCol[col]++;
+            board = tempBoard;
+            column = tempCol;
+
+            return true;
         }
     }
 
-    public int[][] getBoard() {return board;}
-    public boolean getGameOver() {return gameOver;}
-    public int getTurn() {return turn;}
-    public int[] getColumn() {return column;}
+    //check for a vertical win...
+    public boolean verticalWin() {
+
+        int count = 0;
+        int piece = EMPTY;
+        if (turn == 0) {
+            piece = RED;
+        } else if (turn == 1) {
+            piece = BLACK;
+        }
+
+        //look through the entire board
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                //if we find a piece, up the count
+                if (board[i][j] == piece) {
+                    count++;
+                    //we found four in a row, you won!
+                    if (count >= 4) return true;
+                }
+                //else reset the count to zero
+                else {
+                    count = 0;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    //check for a horizontal win...
+    public boolean horizontalWin() {
+
+        int count = 0;
+        int piece = EMPTY;
+        if (turn == 0) {
+            piece = RED;
+        } else if (turn == 1) {
+            piece = BLACK;
+        }
+        int tempRow = 0;
+
+        //look through the entire board
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                //if we are in the first column
+                if (i == 0) {
+                    //if we find a piece, get the row
+                    //up the count
+                    //set j to the max so we start to look at the next column
+                    if (board[i][j] == piece) {
+                        tempRow = j;
+                        count++;
+                        j = board[i].length;
+                    }
+                }
+                //if we aren't in the first column
+                else if (i > 0) {
+                    //if there's a piece in the tempRow
+                    //up the count
+                    if (board[i][tempRow] == piece) {
+                        count++;
+                        j = board[i].length;
+                        //we found four in a row, you won!
+                        if (count >= 4) return true;
+                    }
+                    //else reset the count to 0
+                    else {
+                        count = 0;
+                    }
+                }
+
+            }
+
+        }
+        return false;
+    }
+
+    //check for diagonal win
+    public boolean diagonalWin() {
+        int count = 0;
+        int piece = EMPTY;
+        if (turn == 0) {
+            piece = RED;
+        } else if (turn == 1) {
+            piece = BLACK;
+        }
+        int tempRow = 0;
+        int tempCol = 0;
+
+        //look through the board
+        //checks for bottom-left to top-right diagonal
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == piece) {
+                    //get temp col and row
+                    tempCol = i;
+                    tempRow = j;
+                    //loop that checks for, at most, four pieces in a row
+                    for (int a = 0; a < 4; a++) {
+                        if (tempCol + 1 < board.length && tempRow + 1 < board[i].length) {
+                            //if it's the right piece, do this stuff
+                            if (board[tempCol][tempRow] == piece) {
+                                count++;
+                                tempCol++;
+                                tempRow++;
+                                //we found four in a row, you won!
+                                if (count >= 4) {
+                                    return true;
+                                }
+                            }
+                            //reset count to zero
+                            else {
+                                count = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //similar to the double for loop above
+        //checks for top-left to bottom-right diagonal
+        for (int i = board.length - 1; i >= 0; i--) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == piece) {
+                    tempCol = i;
+                    tempRow = j;
+                    for (int a = 0; a < 4; a++) {
+                        if (tempCol - 1 >= 0 && tempRow + 1 < board[i].length) {
+                            if (board[tempCol][tempRow] == piece) {
+                                count++;
+                                tempCol--;
+                                tempRow++;
+                                if (count >= 4) {
+                                    return true;
+                                }
+
+                            }
+                            else {
+                                count = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    //checks if the game is over...
+    public boolean gameOver() {
+
+        if (verticalWin() || horizontalWin() || diagonalWin()) {
+            return true;
+        } else {
+            if (turn == 0)
+            {
+                turn = 1;
+            }
+            else {
+                turn = 0;
+            }
+            return false;
+        }
+
+    }
+
+    //gets the board...
+    public int[][] getBoard() {
+        return board;
+    }
+
+    //gets gameOver boolean...
+    public boolean getGameOver() {
+        return gameOver;
+    }
+
+    //gets the turn...
+    public int getTurn() {
+        return turn;
+    }
+
+    //gets the column array
+    public int[] getColumn() {
+        return column;
+    }
 }
--- app/src/test/java/edu/up/cs301/ConnectFour/ConnectFourStateTest.java
+++ app/src/test/java/edu/up/cs301/ConnectFour/ConnectFourStateTest.java
@@ -10,47 +10,224 @@ import static org.junit.Assert.*;
 public class ConnectFourStateTest {
 
     @Test
+    //test the getPiece method
     public void testGetPiece() throws Exception {
         ConnectFourState testState = new ConnectFourState();
+        //empty spot at 1,1
+        //get what is at 1,1
         int testThis = testState.getPiece(1,1);
+        //assert that there is no piece in 1,1
         assertEquals(testThis, 0);
     }
 
     @Test
+    //test setPiece method
     public void testSetPiece() throws Exception {
         ConnectFourState testState = new ConnectFourState();
-        testState.setPiece(1, 1, 0);
-        int testThis = testState.getPiece(1, 1);
+        //set pieces at column 0
+        //gameOver method switches player for now
+        testState.setPiece(0);
+        testState.gameOver();
+        testState.setPiece(0);
+        testState.gameOver();
+        testState.setPiece(0);
+        testState.gameOver();
+        testState.setPiece(0);
+        testState.gameOver();
+        testState.setPiece(0);
+        testState.gameOver();
+        testState.setPiece(0);
+        testState.gameOver();
+
+        //check if the pieces go out of bounds
+        boolean test = testState.setPiece(0);
+        int testThis = testState.getPiece(0, 2);
+        assertEquals(test, false);
         assertEquals(testThis, 1);
     }
 
     @Test
+    //test getBoard method
     public void testGetBoard() throws Exception {
         ConnectFourState testState = new ConnectFourState();
-        testState.setPiece(1, 1, 0);
+        //set piece in column 1
+        testState.setPiece(1);
+        testState.gameOver();
         int[][] testThisBoard; //= new int[6][7];
         testThisBoard = testState.getBoard();
-        assertEquals(testThisBoard[1][1], 1);
+
+        //assert that there is one piece in column 1
+        assertEquals(testThisBoard[1][0], 1);
     }
 
     @Test
+    //test getGameOver method
     public void testGetGameOver() throws Exception {
         ConnectFourState testState = new ConnectFourState();
+        //if no pieces are played, game over should be false
         boolean testThis = testState.getGameOver();
         assertEquals(testThis, false);
 
     }
 
     @Test
+    //test gameOver method
+    public void testGameOver() throws Exception {
+        ConnectFourState testState = new ConnectFourState();
+        //set pieces so that there is a horizontal win
+        testState.setPiece(1); //r
+        testState.gameOver();
+        testState.setPiece(1); //b
+        testState.gameOver();
+        testState.setPiece(2); //r
+        testState.gameOver();
+        testState.setPiece(2); //b
+        testState.gameOver();
+        testState.setPiece(3); //r
+        testState.gameOver();
+        testState.setPiece(3); //b
+        testState.gameOver();
+        testState.setPiece(4); //r
+        testState.gameOver();
+        testState.setPiece(0); //b
+        testState.gameOver();
+        //game should be over, boolean will be true if the game is over
+        boolean test = testState.gameOver();
+        assertEquals(test, true);
+
+
+    }
+
+    @Test
+    //test diagonalWin method
+    public void testDiagonalWin() throws Exception {
+        ConnectFourState testState = new ConnectFourState();
+        //test diagonal pieces placed bottom to top, left to right
+        testState.setPiece(1); //r
+        testState.gameOver();
+        testState.setPiece(2); //b
+        testState.gameOver();
+        testState.setPiece(2); //r
+        testState.gameOver();
+        testState.setPiece(3); //b
+        testState.gameOver();
+        testState.setPiece(3); //r
+        testState.gameOver();
+        testState.setPiece(4); //b
+        testState.gameOver();
+        testState.setPiece(3); //r
+        testState.gameOver();
+        testState.setPiece(4); //b
+        testState.gameOver();
+        testState.setPiece(4); //r
+        testState.gameOver();
+        testState.setPiece(0); //b
+        testState.gameOver();
+        testState.setPiece(4); //r
+        testState.gameOver();
+        boolean test = testState.diagonalWin();
+        assertEquals(test, true);
+
+
+        ConnectFourState testState3 = new ConnectFourState();
+        //test diagonal pieces placed top to bottom, right to left
+        testState3.setPiece(4); //r
+        testState.gameOver();
+        testState3.setPiece(3); //b
+        testState.gameOver();
+        testState3.setPiece(3); //r
+        testState.gameOver();
+        testState3.setPiece(2); //b
+        testState.gameOver();
+        testState3.setPiece(2); //r
+        testState.gameOver();
+        testState3.setPiece(1); //b
+        testState.gameOver();
+        testState3.setPiece(2); //r
+        testState.gameOver();
+        testState3.setPiece(1); //b
+        testState.gameOver();
+        testState3.setPiece(1); //r
+        testState.gameOver();
+        testState3.setPiece(0); //b
+        testState.gameOver();
+        testState3.setPiece(1); //r
+        testState.gameOver();
+        boolean test21 = testState.diagonalWin();
+        assertEquals(test21, true);
+
+
+    }
+
+    @Test
+    //test horizontalWin method
+    public void testHorizontalWin() throws Exception {
+        ConnectFourState testState = new ConnectFourState();
+
+        //set pieces in a horizontal pattern
+        //again, gameOver makes sure the next player gets to set a piece
+        testState.setPiece(1); //r
+        testState.gameOver();
+        testState.setPiece(1); //b
+        testState.gameOver();
+        testState.setPiece(2); //r
+        testState.gameOver();
+        testState.setPiece(2); //b
+        testState.gameOver();
+        testState.setPiece(3); //r
+        testState.gameOver();
+        testState.setPiece(3); //b
+        testState.gameOver();
+        testState.setPiece(4); //r
+        testState.gameOver();
+        boolean test = testState.horizontalWin();
+        assertEquals(test, true);
+
+    }
+
+    @Test
+    //test verticalWin method
+    public void testVerticalWin() throws Exception {
+        ConnectFourState testState = new ConnectFourState();
+
+        //set pieces in a vertical arrangement
+        testState.setPiece(3); //r
+        testState.gameOver();
+        testState.setPiece(2); //b
+        testState.gameOver();
+        testState.setPiece(3); //r
+        testState.gameOver();
+        testState.setPiece(2); //b
+        testState.gameOver();
+        testState.setPiece(3); //r
+        testState.gameOver();
+        testState.setPiece(2); //b
+        testState.gameOver();
+        testState.setPiece(3); //r
+        testState.gameOver();
+        testState.setPiece(4); //b
+        testState.gameOver();
+
+        boolean test = testState.verticalWin();
+        assertEquals(test, true);
+
+    }
+
+    @Test
+    //test getTurn method
     public void testGetTurn() throws Exception {
         ConnectFourState testState = new ConnectFourState();
+
+        //player 0 goes first, test to make sure it is player 0s turn
         int testThis = testState.getTurn();
         assertEquals(testThis, 0);
     }
 
     @Test
+    //test getColumn method
     public void testGetColumn() throws Exception {
         ConnectFourState testState = new ConnectFourState();
+        //no pieces have been set, that means there should be a zero in the first column array value
         int[] testCol = testState.getColumn();
         assertEquals(testCol[0], 0);
     }
