f5c0023102aa0f2cc46cb657cd86370f6421f5f2
timothma <macnary17@up.edu>
2015-12-04 08:17:43 -0800

    UndoAction and ResetAction didn't get uploaded to git. they should be there now.

---
+++ app/src/main/java/edu/up/cs301/ConnectFour/ResetAction.java
@@ -0,0 +1,15 @@
+package edu.up.cs301.ConnectFour;
+
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
+/**
+ * Created by macnary17 on 12/3/2015.
+ */
+public class ResetAction extends GameAction{
+
+    public ResetAction(GamePlayer player) {
+        super(player);
+    }
+
+}
---
+++ app/src/main/java/edu/up/cs301/ConnectFour/UndoAction.java
@@ -0,0 +1,13 @@
+package edu.up.cs301.ConnectFour;
+
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+/**
+ * Created by macnary17 on 12/3/2015.
+ */
+public class UndoAction extends GameAction {
+
+    public UndoAction(GamePlayer player) {
+        super(player);
+    }
+}
