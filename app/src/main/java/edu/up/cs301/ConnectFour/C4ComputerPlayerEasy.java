8b4bc4be73d2274df1a3c43bde78bc8cd4db4579
phamj17 <phamj17@up.edu>
2015-12-09 12:55:17 -0800

    Easy AI doneskis.

--- app/src/main/java/edu/up/cs301/ConnectFour/C4ComputerPlayerEasy.java
+++ app/src/main/java/edu/up/cs301/ConnectFour/C4ComputerPlayerEasy.java
@@ -25,9 +25,7 @@ public class C4ComputerPlayerEasy extends GameComputerPlayer {
         //Check if opponent can get 4 in a row
         if (info instanceof ConnectFourState) {
             newState = (ConnectFourState) info;
-            for (int i = 0; i < 100; i++) {
-                board = newState.getBoard();
-            }
+            board = newState.getBoard();
             int playerIdx = newState.getTurn();
         }
 
@@ -45,9 +43,9 @@ public class C4ComputerPlayerEasy extends GameComputerPlayer {
         if (stopVerticalWin() != -1) {
             stopHuman = stopVerticalWin();
         }
-//        else if (stopDiagonalWin()!=-1) {
-//            stopHuman = stopDiagonalWin();
-//        }
+        else if (stopDiagonalWin()!=-1) {
+            stopHuman = stopDiagonalWin();
+        }
         else if (stopHorizontalWin() != -1) {
             stopHuman = stopHorizontalWin();
         }
@@ -59,8 +57,7 @@ public class C4ComputerPlayerEasy extends GameComputerPlayer {
         if (stopHuman != -1) {
             move = stopHuman;
         } else {
-            //move = rand.nextInt(7);
-            move = 6;
+            move = rand.nextInt(7);
         }
 
         //If opponent cant get 4 in a row, move randomly
@@ -92,13 +89,6 @@ public class C4ComputerPlayerEasy extends GameComputerPlayer {
     public int stopVerticalWin() {
         int count = 0;
         int piece = newState.getRED();
-//        if (newState.getTurn() == 1) {
-//            piece = newState.getRED();
-//        } else if (newState.getTurn() == 0) {
-//            piece = newState.getBLACK();
-//        }
-        //int tempRow = -1;
-        //int missingCol;
         int tempCol;
 
         for (int i = board.length - 1; i >= 0; i--) {
@@ -123,31 +113,6 @@ public class C4ComputerPlayerEasy extends GameComputerPlayer {
                 }
             }
         }
-
-//        for (int i = board.length - 1; i >= 3; i--) {
-//            for (int j = 0; j <= board[i].length - 1; j++) {
-//                count = 0;
-//                tempRow = -1;
-//                for (int a = 0; a < 3; a++) {
-//                    if (board[i - a][j] == piece) {
-//                        count++;
-//                        tempRow = i - a;
-//                    }
-//                    else {count = 0;}
-//                }
-//
-//                if (count == 3) {
-//                    if (tempRow > 0) {
-//                        if (board[tempRow + 1][j] != newState.getEMPTY()) {
-//                            return j;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-
-
-
         return -1;
     }
 
@@ -155,11 +120,6 @@ public class C4ComputerPlayerEasy extends GameComputerPlayer {
 
         int count = 0;
         int piece = newState.getRED();
-//        if (newState.getTurn() == 1) {
-//            piece = newState.getRED();
-//        } else if (newState.getTurn() == 0) {
-//            piece = newState.getBLACK();
-//        }
 
         int missingCol = -1;
 
@@ -188,49 +148,11 @@ public class C4ComputerPlayerEasy extends GameComputerPlayer {
         return -1;
 
     }
-//        for (int i = board.length - 1; i >= 0; i--) {
-//            for (int j = 0; j < board[i].length; j++) {
-//                count = 0;
-//                if (board[i][j] == piece) {
-//                    if (j <= 5) {
-//                        tempRow = i;
-//                        for (int a = 0; a < 2; a++) {
-//                            if (board[tempRow][j + a] == piece) {
-//                                count++;
-//                                if (count >= 2) {
-//                                    if (j + a + 1 < 7 && board[tempRow][j + a + 1] == newState.getEMPTY()) {
-//                                        if (tempRow + 1 >= 6 || board[tempRow + 1][j + a + 1] != newState.getEMPTY()) {
-//                                            return j + a + 1;
-//                                        }
-//                                    }
-//                                    if (j - 1 >= 0 && board[tempRow][j - 1] == newState.getEMPTY()) {
-//                                        if (tempRow + 1 >= 6 || board[tempRow + 1][j - 1] != newState.getEMPTY()) {
-//                                            return j - 1;
-//                                        }
-//                                    }
-//                                }
-//                            } else {
-//                                count = 0;
-//                                a = 3;
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//        }
-//        return -1;
-
 
     public int stop2Horizontal() {
 
         int count = 0;
         int piece = newState.getRED();
-//        if (newState.getTurn() == 1) {
-//            piece = newState.getRED();
-//        } else if (newState.getTurn() == 0) {
-//            piece = newState.getBLACK();
-//        }
 
         int missingCol = -1;
 
@@ -267,127 +189,69 @@ public class C4ComputerPlayerEasy extends GameComputerPlayer {
             }
         }
         return -1;
-
     }
 
-//    public int stopDiagonalWin() {
-//
-//        int count = 0;
-//        int missingCol = -1;
-//        int tempRow = -1;
-//        int piece = newState.getRED();
-//
-//        for (int i = board.length - 4; i >= 0; i--) {
-//            for (int j = 0; j <= board.length - 4; j++) {
-//                count = 0;
-//                missingCol = -1;
-//                for (int a = 0; a < 4; a++) {
-//                    if (board[i - a][j + a] == piece) {count++;} //something wrong with array exceptions
-//                    else if(board[i - a][j + a] == newState.getEMPTY()) {
-//                        missingCol = j + a;
-//                        tempRow = i - a;
-//                    }
-//                    else {
-//                        missingCol = -1;
-//                        tempRow = -1;
-//                    }
-//                }
-//
-//                if (count == 3 && missingCol != -1) {
-//                    if (tempRow < board.length - 1) {
-//                        if (board[tempRow + 1][missingCol] != newState.getEMPTY()) {
-//                            return missingCol;
-//                        }
-//                    }
-//                    else if (tempRow == board.length - 1) {
-//                        return missingCol;
-//                    }
-//                }
-//            }
-//        }
-//
-//        return -1;
-//    }
+    public int stopDiagonalWin() {
 
-//    public int stopDiagonalWin() {
-//        boolean keepGoing = false;
-//        int count = 0;
-//        int piece = newState.getEMPTY();
-//        if (newState.getTurn() == 1) {
-//            piece = newState.getRED();
-//        } else if (newState.getTurn() == 0) {
-//            piece = newState.getBLACK();
-//        }
-//
-//
-//        //look through entire game board, starting in top left corner
-//        for (int i = 0; i < board.length; i++) {
-//            for (int j = 0; j < board[i].length; j++) {
-//                if (board[i][j] == piece) {
-//                    count = 1;
-//                    int k = i;
-//                    int l = j;
-//                    keepGoing = true;
-//                    if (i < 4 && j < 5) {
-//                        while (keepGoing) {
-//                            k++;
-//                            l++;
-//                            if (board[k][l] == piece) {
-//                                count++;
-//                            } else {
-//                                keepGoing = false;
-//                                count = 0;
-//                            }
-//                            if (count == 3) {
-//                                keepGoing = false;
-//                                if (i >= 1 && j >= 1) {
-//                                    if (board[i - 1][j - 1] == newState.getEMPTY() && board[i][j - 1] != newState.getEMPTY()) {
-//                                        return j - 1;
-//                                    }
-//                                }
-//                                if (k + 1 <= 5 && board[k + 1][l + 1] == newState.getEMPTY() && (k+2>5 || board[k + 1][l + 1] != newState.getEMPTY())) {
-//                                    return l + 1;
-//                                }
-////                                else if (i < 3 && j < 4) {
-////                                    if (k + 2 > 5 && board[k + 1][l + 1] == newState.getEMPTY()) {
-////                                        return l + 1;
-////                                    }
-////                                }
-//
-//                            }
-//                        }
-//                    }
-//                }
-//                count = 0;
-//            }
-//        }
-//        for (int i = 0; i < board.length; i++) {
-//            for (int j = 0; j < board[i].length; j++) {
-//                if (board[i][j] == piece) {
-//                    count = 1;
-//                    int k = i;
-//                    int l = j;
-//                    keepGoing = true;
-//                    if (j > 2 && i < 3) {
-//                        while (keepGoing) {
-//                            k++;
-//                            l--;
-//                            if (board[k][l] == piece) {
-//                                count++;
-//                                if (count == 4) {
-//                                    return true;
-//                                }
-//                            } else {
-//                                keepGoing = false;
-//                                count = 0;
-//                            }
-//
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    return -1;
-//    }
+        int count = 0;
+        int missingCol = -1;
+        int tempRow = -1;
+        int piece = newState.getRED();
 
+        for (int i = 0; i <= board.length - 4; i++) {
+            for (int j = 0; j <= board[i].length - 4; j++) {
+                count = 0;
+                missingCol = -1;
+                for (int a = 0; a < 4; a++) {
+                    if (board[i + a][j + a] == piece) {count++;}
+                    else if(board[i + a][j + a] == newState.getEMPTY()) {
+                        missingCol = j + a;
+                        tempRow = i + a;
+                    }
+                    else {
+                        missingCol = -1;
+                        tempRow = -1;
+                    }
+                }
+                if (count == 3 && missingCol != -1) {
+                    if (tempRow < board.length - 1) {
+                        if (board[tempRow + 1][missingCol] != newState.getEMPTY()) {
+                            return missingCol;
+                        }
+                    }
+                    else if (tempRow == board.length - 1) {
+                        return missingCol;
+                    }
+                }
+            }
+        }
+        for (int i = board.length - 1; i >= 3; i--) {
+            for (int j = 0; j <= board[i].length - 4; j++) {
+                count = 0;
+                missingCol = -1;
+                for (int a = 0; a < 4; a++) {
+                    if (board[i - a][j + a] == piece) {count++;}
+                    else if(board[i - a][j + a] == newState.getEMPTY()) {
+                        missingCol = j + a;
+                        tempRow = i - a;
+                    }
+                    else {
+                        missingCol = -1;
+                        tempRow = -1;
+                    }
+                }
+                if (count == 3 && missingCol != -1) {
+                    if (tempRow < board.length - 1) {
+                        if (board[tempRow + 1][missingCol] != newState.getEMPTY()) {
+                            return missingCol;
+                        }
+                    }
+                    else if (tempRow == board.length - 1) {
+                        return missingCol;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
 }
